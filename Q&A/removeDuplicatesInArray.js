9f0c69830cfeec3afb5164ae57b0873f336e566d
niranjangutha <niranjangutha4@gmail.com>
2025-01-29 08:49:32 +0530

    remove the duplicates in array of objects

--- Q&A/removeDuplicatesInArray.js
+++ Q&A/removeDuplicatesInArray.js
@@ -1,5 +1,6 @@
-let data = [1,2,3,4,5,6,6,2,3,2,3,2,3,3,3,5,4,3,2,4,5,6,7];
-//let data = ["a","b","c","d","x","v","b","f","s","a","x","c","b","b","f","b","a","a","a","v","b","c"];
+//let data = [1,2,3,4,5,6,6,2,3,2,3,2,3,3,3,5,4,3,2,4,5,6,7];
+let data = ["a","b","c","d","x","v","b","f","s","a","x","c","b","b","f","b","a","a","a","v","b","c"];
+
 
 //! REmove the Duplicate data in an array  using spread(...) and set method.
 
@@ -15,3 +16,22 @@ data.forEach((val)=>{
     }
 })
 console.log(distinctvalues);
+
+
+//! Remove the Duplicates in array of objects.
+const array = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 1, name: 'Alice' }, // Duplicate
+    { id: 3, name: 'Charlie' }
+];
+
+// Remove duplicates based on 'id'
+const uniqueArray = array.reduce((accumulator, currentValue) => {
+    if (!accumulator.some(item => item.id === currentValue.id)) {
+        accumulator.push(currentValue);
+    }
+    return accumulator;
+}, []);
+
+console.log(uniqueArray);
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ most frequently asked interview questions
 
 28/01/2025
 Q : How to find the smalest word in the given string.(ref: smallString.js)
+
+29/01/2025
+Q : How to remove the duplicate values in the given array. (ref: removeDuplicatesInArray.js )
